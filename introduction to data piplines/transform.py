45846733e9b03f875a8fa696a1ea8b57f7b06c4c
AhmadDaw <121485060+AhmadDaw@users.noreply.github.com>
2024-04-17 07:20:17 +0300

    Update transform.py

--- introduction to data piplines/transform.py
+++ introduction to data piplines/transform.py
@@ -1,6 +1,3 @@
-# Extract data from the sales_data.parquet path
-raw_sales_data = extract("sales_data.parquet")
-
 def transform(raw_data):
   	# Only keep rows with `Quantity Ordered` greater than 1
     clean_data = raw_data.loc[raw_data["Quantity Ordered"]>1, :]
@@ -11,4 +8,3 @@ def transform(raw_data):
     # Return the filtered DataFrame
     return clean_data
     
-transform(raw_sales_data)
