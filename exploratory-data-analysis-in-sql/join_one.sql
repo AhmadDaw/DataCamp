4bb789039996e845a87a60267a8eca89a3f11cc5
AhmadDaw <121485060+AhmadDaw@users.noreply.github.com>
2024-05-13 07:11:02 +0300

    Add files via upload

---
+++ exploratory-data-analysis-in-sql/join_one.sql
@@ -0,0 +1,20 @@
+SELECT company.name
+-- Table(s) to select from
+  FROM company
+       INNER JOIN fortune500
+       ON company.ticker = fortune500.ticker;
+
+-- Select the 3 columns desired
+SELECT company.name, tag_type.tag, tag_type.type
+  FROM company
+  	   -- Join to the tag_company table
+       INNER JOIN tag_company 
+       ON company.id = tag_company.company_id
+       -- Join to the tag_type table
+       INNER JOIN tag_type
+       ON tag_company.tag = tag_type.tag
+  -- Filter to most common type
+  WHERE type='cloud';
+
+  
+       
